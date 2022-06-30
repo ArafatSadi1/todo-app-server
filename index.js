8aa11aa9ef53eb7d395589ddb028916053614758
ArafatSadi1 <arafatsadi1@gmail.com>
2022-06-30 23:49:15 +0600

    update single todo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT | 5000;
 
 // middleware
@@ -11,31 +11,53 @@ app.use(express.json());
 // const userDB = todo-data
 // const pass = om9AneO3LTWllaA7
 
-const uri = "mongodb+srv://todo-data:<om9AneO3LTWllaA7>@cluster0.locwg.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const uri =
+  "mongodb+srv://todo-data:om9AneO3LTWllaA7@cluster0.locwg.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try {
-      await client.connect();
-      const todoCollection = client.db("todos").collection("todo");
+  try {
+    await client.connect();
+    const todoCollection = client.db("todos").collection("todo");
 
-      app.post('/todo', async(req, res)=>{
-        const todo = req.body;
-        const result = await todoCollection.insertOne(todo);
-        res.send(result)
-      })
+    app.post("/todo", async (req, res) => {
+      const todo = req.body;
+      const result = await todoCollection.insertOne(todo);
+      res.send(result);
+    });
 
-    } finally {
-    }
+    app.get("/todos", async (req, res) => {
+      const result = await todoCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    app.patch("/todo/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const filter = { _id: ObjectId(id)};
+      const {updatedTodo} = req.body;
+      console.log(updatedTodo)
+      const updatedDoc = {
+        $set: { 
+            todo: updatedTodo
+        }
+      }
+      const result = await todoCollection.updateOne(filter, updatedDoc);
+      res.send(result)
+    });
+  } finally {
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello from todoist app')
-})
+app.get("/", (req, res) => {
+  res.send("Hello from todoist app");
+});
 
 app.listen(port, () => {
-  console.log(`Todo app listening on port ${port}`)
-})
+  console.log(`Todo app listening on port ${port}`);
+});
